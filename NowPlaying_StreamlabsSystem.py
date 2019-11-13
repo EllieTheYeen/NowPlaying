b464aa877552b8aeea2000dc56d606fd69d84be7
Ellie The Yeen <42704150+EllieTheYeen@users.noreply.github.com>
2019-11-13 20:12:36 +0100

    First commit

---
+++ NowPlaying_StreamlabsSystem.py
@@ -0,0 +1,38 @@
+import time
+
+
+ScriptName = "Now playing"
+Website = "https://github.com/GoatTGirl"
+Description = "Post current playing song name in chat"
+Creator = "GoatTGirl"
+Version = "1.0.0.0"
+
+# Just to make linter not complain
+try:
+        Parent
+except:
+        Parent = None
+
+lasttick = None
+lastsong = None
+
+def Init(): pass
+    
+def Execute(data): pass
+    
+def Tick():
+    global lasttick
+    nowtick = int(time.time())
+    if not lasttick:
+        lasttick = nowtick
+    if abs(nowtick - lasttick) >= 5:
+        lasttick = nowtick
+        SecondsTick()
+    
+def SecondsTick():
+    global lastsong
+    current = Parent.GetNowPlaying()
+    currentsong = current.Key
+    if currentsong and currentsong != lastsong:
+        lastsong = currentsong
+        Parent.SendStreamMessage('Now playing: %s' % (currentsong,))
